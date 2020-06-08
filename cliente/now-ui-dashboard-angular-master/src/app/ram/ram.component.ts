a1918ed34e1a0ea5cd80fb35ce25121521ed134d
jeannirasic <jeannirasic@gmail.com>
2020-06-07 20:32:39 -0600

    Paginas de ram y cpu terminadas

--- cliente/now-ui-dashboard-angular-master/src/app/ram/ram.component.ts
+++ cliente/now-ui-dashboard-angular-master/src/app/ram/ram.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-ram',
   templateUrl: './ram.component.html',
@@ -7,9 +6,141 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RamComponent implements OnInit {
 
+  ramServidor = 0;
+  ramConsumida = 0;
+  porcentajeRam = 0;
+
+  public lineBigDashboardChartType;
+  public gradientStroke;
+  public chartColor;
+  public canvas: any;
+  public ctx;
+  public gradientFill;
+  public lineBigDashboardChartData: Array<any>;
+  public lineBigDashboardChartOptions: any;
+  public lineBigDashboardChartLabels: Array<any>;
+  public lineBigDashboardChartColors: Array<any>;
+
+
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+  public hexToRGB(hex, alpha) {
+    const r = parseInt(hex.slice(1, 3), 16),
+      g = parseInt(hex.slice(3, 5), 16),
+      b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+      return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + alpha + ')';
+    } else {
+      return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    }
+  }
   constructor() { }
 
   ngOnInit() {
+    this.chartColor = '#FFFFFF';
+    this.canvas = document.getElementById('bigDashboardChart');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.gradientStroke = this.ctx.createLinearGradient(500, 0, 100, 0);
+    this.gradientStroke.addColorStop(0, '#80b6f4');
+    this.gradientStroke.addColorStop(1, this.chartColor);
+
+    this.gradientFill = this.ctx.createLinearGradient(0, 200, 0, 50);
+    this.gradientFill.addColorStop(0, 'rgba(128, 182, 244, 0)');
+    this.gradientFill.addColorStop(1, 'rgba(255, 255, 255, 0.24)');
+
+    this.lineBigDashboardChartData = [
+        {
+          label: 'Data',
+
+          pointBorderWidth: 1,
+          pointHoverRadius: 7,
+          pointHoverBorderWidth: 2,
+          pointRadius: 5,
+          fill: true,
+          lineTension: 0,
+          borderWidth: 2,
+          data: [50, 50, 100, 90, 30, 90, 50, 60, 20, 40, 90, 95]
+        }
+      ];
+      this.lineBigDashboardChartColors = [
+       {
+         backgroundColor: this.gradientFill,
+         borderColor: this.chartColor,
+         pointBorderColor: this.chartColor,
+         pointBackgroundColor: '#2c2c2c',
+         pointHoverBackgroundColor: '#2c2c2c',
+         pointHoverBorderColor: this.chartColor,
+       }
+     ];
+    this.lineBigDashboardChartLabels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
+    this.lineBigDashboardChartOptions = {
+
+          layout: {
+              padding: {
+                  left: 20,
+                  right: 20,
+                  top: 0,
+                  bottom: 0
+              }
+          },
+          maintainAspectRatio: false,
+          tooltips: {
+            backgroundColor: '#fff',
+            titleFontColor: '#333',
+            bodyFontColor: '#666',
+            bodySpacing: 4,
+            xPadding: 12,
+            mode: 'nearest',
+            intersect: 0,
+            position: 'nearest'
+          },
+          legend: {
+              position: 'bottom',
+              fillStyle: '#FFF',
+              display: false
+          },
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      fontColor: 'rgba(255,255,255,0.4)',
+                      fontStyle: 'bold',
+                      beginAtZero: true,
+                      maxTicksLimit: 5,
+                      padding: 10
+                  },
+                  gridLines: {
+                      drawTicks: true,
+                      drawBorder: false,
+                      display: true,
+                      color: 'rgba(255,255,255,0.1)',
+                      zeroLineColor: 'transparent'
+                  }
+
+              }],
+              xAxes: [{
+                  gridLines: {
+                      zeroLineColor: 'transparent',
+                      display: false,
+
+                  },
+                  ticks: {
+                      padding: 10,
+                      fontColor: 'rgba(255,255,255,0.4)',
+                      fontStyle: 'bold'
+                  }
+              }]
+          }
+    };
+
+    this.lineBigDashboardChartType = 'line';
+
   }
 
 }
