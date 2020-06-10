2a21326c35bb8361c2481432b2528d1cb6990684
Feruiz12000 <feruiz.12000@gmail.com>
2020-06-09 21:09:22 -0600

    Modulos Finalizados

--- Modulos/ModuloCPU/cpu_201602434_201503984.c
+++ Modulos/ModuloCPU/cpu_201602434_201503984.c
@@ -11,10 +11,60 @@
 #include <linux/sched/signal.h> 
 
 
+
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Escribir informacion del cpu.");
 MODULE_AUTHOR("Jeannira Del Rosario Sic Menéndez - 201602434\nFernando Vidal Ruiz Piox -  201503984");
 
+int cont = 0;
+void dfs(struct task_struct *task, struct seq_file * m, int num)
+{
+  struct task_struct *task_next;
+  struct list_head *list;
+  
+  int tmp = cont;
+  cont = cont + num;
+  
+  list_for_each(list, &task->children) {
+    task_next = list_entry(list, struct task_struct, sibling);
+
+    if(task_next == 0){
+        continue;
+    }
+    int x;
+    for(x = 0; x<cont;x+=1)
+    {
+        seq_printf(m,"|     ");
+    }
+    seq_printf(m,"├── ");
+    
+    //============= PID ==================
+    seq_printf(m,"PID:%d       ",task_next->pid);
+    //seq_printf(m,"%s","	");
+	//============ NOMBRE ==================
+    seq_printf(m,"Nombre:%s       ",task_next->comm);
+    //seq_printf(m,"%s","			");
+    //============ ESTADO ==================
+    if(task_next->state == -1){
+        seq_printf(m,"Estado:NOT EXECUTABLE");
+    }else if(task_next->state == 0){
+        seq_printf(m,"Estado:RUNNING");
+    }else if(task_next->state == 1){
+        seq_printf(m,"Estado:INTERRUPTIBLE");
+    }else if(task_next->state == 2){
+        seq_printf(m,"Estado:UNINTERRUPTIBLE");
+    }else if(task_next->state == 4){
+        seq_printf(m,"Estado:ZOMBIE");
+    }else if(task_next->state == 8){
+        seq_printf(m,"Estado:STOPPED");
+    }else{
+        seq_printf(m,"Estado:EXCLUSIVE");
+    }
+	seq_printf(m,"\n");
+    dfs(task_next, m, 1);
+  }
+  cont = tmp;
+}
 
 static int escribir_archivo(struct seq_file * m, void *v) {	
     seq_printf(m, " __________________________________________________\n");
@@ -25,30 +75,10 @@ static int escribir_archivo(struct seq_file * m, void *v) {
     seq_printf(m, "|                                                  |\n");
     seq_printf(m, "|      PROYECTO (MODULO 2 - PROCESOS CPU)          |\n");
     seq_printf(m, "|__________________________________________________|\n");
-    seq_printf(m, "  Sistema Operativo: Ubuntu 18.4\n");
+    seq_printf(m, "        Sistema Operativo: Ubuntu 18.4\n\n");
     
-    
-    struct task_struct* task_list;
-	seq_printf(m,"%s","Processes:\n");
-
-    seq_printf(m,"PID ");
-    seq_printf(m,"%s","	");
-	seq_printf(m,"NOMBRE");
-    seq_printf(m,"%s","			");
-	seq_printf(m,"ESTADO");	
-    seq_printf(m,"%s","\n");
-	
-    for_each_process(task_list) {
-		//============= PID ==================
-        seq_printf(m,"%d",task_list->pid);
-        seq_printf(m,"%s","	");
-		//============ NOMBRE ==================
-        seq_printf(m,"%s",task_list->comm);
-        seq_printf(m,"%s","			");
-        //============ ESTADO ==================
-		seq_printf(m,"%s",task_list->state);
-        seq_printf(m,"%lu","\n");
-    }
+    cont = 0;
+    dfs(&init_task, m, 0);
     return 0;
 }
 
