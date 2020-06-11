cfe808916c22acfb3654811d9253672f829658c3
jeannirasic <jeannirasic@gmail.com>
2020-06-10 21:58:34 -0600

    Iniciando con la ram y los procesos del servidor

--- servidor/principal/main.go
+++ servidor/principal/main.go
@@ -3,46 +3,143 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"net/http"
+	"log"
+	"strconv"
+	"strings"
 )
 
-type Pagina struct {
-	Titulo string
-	Cuerpo []byte
+type DatosRam struct {
+	total      float64
+	consumida  float64
+	porcentaje float64
 }
+
+var (
+	monitorRAM = DatosRam{0, 0, 0}
+)
 
 func main() {
-	//Creamos y guardamos una página para que el cliente la pida
-	/*pag1 := &Pagina{Titulo: "Ejemplo", Cuerpo: []byte(
-		"¡Hola personita! Este es el cuerpo de tu página.")}
-	pag1.guardar()*/
-	http.HandleFunc("/view/", manejadorMostrarPagina)
-	fmt.Println("El servidor se encuentra en ejecución")
-	http.ListenAndServe(":8080", nil)
+	leerMeminfo()
+	leerProcesos()
 }
 
-//Manejador de peticiones
-func manejadorMostrarPagina(w http.ResponseWriter, r *http.Request) {
-	titulo := r.URL.Path
-	pagina, _ := cargarPagina(titulo)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>",
-		pagina.Titulo[len("/view/"):], pagina.Cuerpo)
-	fmt.Println("¡Página servida!")
-}
+//FUNCIONES PRINCIPALES--------------------------------------------------------------------------------------------------------------------------------
 
-//Método para guardar página
-func (p *Pagina) guardar() error {
-	nombre := p.Titulo + ".html"
-	return ioutil.WriteFile("./view/"+nombre, p.Cuerpo, 0600)
-}
-
-//Método para cargar página
-func cargarPagina(titulo string) (*Pagina, error) {
-	nombre_archivo := titulo + ".html"
-	fmt.Println("El cliente ha pedido:" + nombre_archivo)
-	cuerpo, err := ioutil.ReadFile("." + nombre_archivo)
+//Esta funcion es utilizada para obtener la informacion de la ram utilizada y el total de la RAM
+func leerMeminfo() {
+	bytesLeidos, err := ioutil.ReadFile("/proc/meminfo")
 	if err != nil {
-		return nil, err
+		fmt.Printf("Error leyendo archivo: %v", err)
+		return
 	}
-	return &Pagina{Titulo: titulo, Cuerpo: cuerpo}, nil
+
+	contenidoArchivo := string(bytesLeidos)
+	archivoCortado := strings.Split(contenidoArchivo, "\n")
+	monitorRAM.total = (limpiarNumero(archivoCortado[0]) / 1000)
+	libre := limpiarNumero(archivoCortado[2]) / 1000
+	monitorRAM.consumida = (monitorRAM.total - libre)
+	monitorRAM.porcentaje = (monitorRAM.consumida / monitorRAM.total) * 100
+	fmt.Printf("Total: %g Mb, Consumida: %g Mb, Porcentaje: %g \n", monitorRAM.total, monitorRAM.consumida, monitorRAM.porcentaje)
+}
+
+//Esta funcion es utilizada para obtener toda la informacion de los procesos. Hace uso del dato del total de la RAM calculado anteriormente
+func leerProcesos() {
+	archivos, err := ioutil.ReadDir("/proc/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, archivo := range archivos {
+		if archivo.IsDir() {
+			nombreCarpeta := archivo.Name()
+			if buscarNumero(nombreCarpeta) {
+				bytesLeidos, err := ioutil.ReadFile("/proc/" + nombreCarpeta + "/status")
+				if err != nil {
+					fmt.Printf("Error leyendo archivo: %v", err)
+					return
+				}
+				contenidoArchivo := string(bytesLeidos)
+				archivoCortado := strings.Split(contenidoArchivo, "\n")
+				fmt.Printf("PID: %s, Nombre: %s, Usuario: %s, Estado: %s, Porcentaje: %g \n", limpiarProc(archivoCortado[5]), limpiarProc(archivoCortado[0]),
+					limpiarUsuario(archivoCortado[8]), limpiarEstado(archivoCortado[2]), (buscarPorcentajeRam(archivoCortado[17])))
+
+			}
+		}
+	}
+
+}
+
+//FUNCIONES AUXILIARES----------------------------------------------------------------------------------------------------------------------------
+
+//Esta funcion devuelve el porcentaje de ram utilizado por cada procesos
+func buscarPorcentajeRam(cadena string) float64 {
+	if strings.HasPrefix(cadena, "VmSize") {
+		tamanioConvertido := limpiarNumero(cadena) / 1000
+		porcentaje := (tamanioConvertido / monitorRAM.total) * 100
+		return porcentaje
+	}
+	return 0
+}
+
+//Esta funcion es utilizada para quitar la informacion innecesaria de una linea del archivo proc
+func limpiarProc(cadena string) string {
+	cortado := strings.Split(cadena, ":")
+	sinEspacios := strings.TrimSpace(cortado[1])
+	return sinEspacios
+}
+
+//Esta funcion es utilizada para quitar informacion innecesaria en una linea que contiene un numero y lo devuelve convertido
+func limpiarNumero(cadena string) float64 {
+	cortado := strings.Split(cadena, ":")
+	sinEspacios := strings.TrimSpace(cortado[1])
+	final := strings.Fields(sinEspacios)
+	retorno, error := strconv.Atoi(final[0])
+	if error != nil {
+		fmt.Println("Error al convertir: ", error)
+		return 0
+	}
+	return float64(retorno)
+}
+
+//Esta funcion busca si el nombre de una carpeta contiene un numero
+func buscarNumero(cadena string) bool {
+	if strings.ContainsAny(cadena, "0") {
+		return true
+	} else if strings.ContainsAny(cadena, "1") {
+		return true
+	} else if strings.ContainsAny(cadena, "2") {
+		return true
+	} else if strings.ContainsAny(cadena, "3") {
+		return true
+	} else if strings.ContainsAny(cadena, "4") {
+		return true
+	} else if strings.ContainsAny(cadena, "5") {
+		return true
+	} else if strings.ContainsAny(cadena, "6") {
+		return true
+	} else if strings.ContainsAny(cadena, "7") {
+		return true
+	} else if strings.ContainsAny(cadena, "8") {
+		return true
+	} else if strings.ContainsAny(cadena, "9") {
+		return true
+	} else {
+		return false
+	}
+}
+
+//Esta funcion devuelve del UID del usuario de un proceso
+func limpiarUsuario(cadena string) string {
+	cortado := strings.Split(cadena, ":")
+	sinEspacios := strings.TrimSpace(cortado[1])
+	final := strings.Fields(sinEspacios)
+	return final[0]
+}
+
+//Esta funcion devuelve solo la letra del estado en el que esta un proceso
+func limpiarEstado(cadena string) string {
+	cortado := strings.Split(cadena, ":")
+	sinEspacios := strings.TrimSpace(cortado[1])
+	final := strings.Fields(sinEspacios)
+	return final[0]
 }
